359dcca97aad043507ba15fa2c45a02039ede79c
Taylor Liss <tayloraliss@gmail.com>
2017-02-27 20:01:20 -0500

    Get colors right and fading for menu button

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,11 +1,15 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
+    document.getElementById("openBar").onclick = function(){closeNav();};
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("openBar").style.color = "#071e57";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
+    document.getElementById("openBar").onclick = function(){openNav();};
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("openBar").style.color = "#D1D1E1";
 }
